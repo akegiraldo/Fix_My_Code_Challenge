cb6a11d768896e684cc6c22050fa9602ce182407
Doouh <ake.giraldo@hotmail.com>
2020-05-21 07:27:20 -0500

    Add all comments

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
+"""
+File to manage of a square
+"""
 
 
 class square:
-
+    """Class of a square"""
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """ Method to inicialize a square with all attributes """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,14 +19,16 @@ class square:
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ Perimeter of the square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ Modify the form to print the square """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-
+    """ Some comment """
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
